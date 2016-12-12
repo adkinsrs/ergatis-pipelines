81cb5142184ec07f03ae4cdef2e551d540c9898f
Shaun Adkins <sadkins@som.umaryland.edu>
2016-12-12 10:55:24 -0500

    created script to create wrappers for exe scripts

---
+++ core/create_wrappers.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+# create_wrappers.sh - Create wrapper files based on the executable scripts present in the bin directory
+# Args - [1] - Base path of Ergatis package
+
+for file in `ls -1 $1/bin`; do
+	fileext=${filename##*.}
+	case $fileext in
+		pl)
+			/usr/bin/perl perl2wrapper_ergatis.pl INSTALL_BASE=$1
+			;;
+		py)
+			/usr/bin/perl python2wrapper_ergatis.pl INSTALL_BASE=$1
+			;;
+		jl)
+			/usr/bin/perl julia2wrapper_ergatis.pl INSTALL_BASE=$1
+			;;
+		*)
+			echo "$file is not a Perl, Python, or Julia file... skipping\n"
+			;;
+	esac
+done
