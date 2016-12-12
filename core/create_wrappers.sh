a006ba11a21cf44b95703b988e98b1bf2a680a15
Shaun Adkins <sadkins@som.umaryland.edu>
2016-12-12 14:33:16 -0500

    adding script_dir variable

--- core/create_wrappers.sh
+++ core/create_wrappers.sh
@@ -3,17 +3,20 @@
 # create_wrappers.sh - Create wrapper files based on the executable scripts present in the bin directory
 # Args - [1] - Base path of Ergatis package
 
+script_dir=$(dirname "$0")
+
+
 for file in `ls -1 $1/bin`; do
 	fileext=${filename##*.}
 	case $fileext in
 		pl)
-			/usr/bin/perl perl2wrapper_ergatis.pl INSTALL_BASE=$1 $file
+			/usr/bin/perl ${script_dir}/perl2wrapper_ergatis.pl INSTALL_BASE=$1 $file
 			;;
 		py)
-			/usr/bin/perl python2wrapper_ergatis.pl INSTALL_BASE=$1 $file
+			/usr/bin/perl ${script_dir}/python2wrapper_ergatis.pl INSTALL_BASE=$1 $file
 			;;
 		jl)
-			/usr/bin/perl julia2wrapper_ergatis.pl INSTALL_BASE=$1 $file
+			/usr/bin/perl ${script_dir}/julia2wrapper_ergatis.pl INSTALL_BASE=$1 $file
 			;;
 		*)
 			echo "$file is not a Perl, Python, or Julia file... skipping\n"
