17f610e960a533d203483e4f4a62fc5891147ad5
Shaun Adkins <sadkins@som.umaryland.edu>
2017-03-21 15:18:47 -0400

    initial writeup of add_read_groups script

--- package-nicu/bin/add_read_group.pl
+++ package-nicu/bin/add_read_group.pl
@@ -2,14 +2,24 @@
 
 =head1 NAME
 
-perl_template.pl - Description
+add_read_group.pl - Wrapper script for Picard's AddOrReplaceReadGroup utility
 
 =head1 SYNOPSIS
 
- USAGE: perl_template.pl
-       --input_file=/path/to/some/input.file
-       --output=/path/to/transterm.file
-     [ --log=/path/to/file.log
+ USAGE: add_read_group.pl
+       --input_file=/path/to/some/input.bam
+       --output_file=/path/to/output.bam
+	   --library=lib1
+	   --platform=illumina
+	   --platform_unit=abc123
+	   --sample_name=sample1
+     [ 
+	   --id=1
+	   --validation_stringency=lenient
+	   --max_records_in_RAM=1000000
+	   --picard_jar=/path/to/picard.jar
+	   --java_path=/path/to/java
+	   --log=/path/to/file.log
        --debug=3
        --help
      ]
@@ -17,8 +27,37 @@ perl_template.pl - Description
 =head1 OPTIONS
 
 B<--input_file,-i>
+	Required. Path to BAM file to serve as input
 
 B<--output_file,-o>
+	Required. File name for output BAM file
+
+B<--id>
+	Optional. Read Group ID number.  Will assign to 1 if not provided.
+
+B<--library>
+	Required. Read Group library string name
+
+B<--platform>
+	Required. Platform the reads originated from (eg. illumina or solid)
+
+B<--platform_unit>
+	Required. Platform unit (eg. run barcode)
+
+B<--sample_name>
+	Required.  Read Group sample name
+
+B<--validation_stringency>
+	Optional.  Validation stringency for all SAM/BAM files read in by Picard Tools.  Possible values are "STRICT" (default if not provided), "LENIENT", or "SILENT"
+
+B<--max_records_stored>
+	Optional.  Number of records to store in RAM before spilling to disk.  Default is 500000.
+
+B<--picard_jar>
+	Optional. Path to picard JAR file.  If not provided, will use /usr/local/packages/picard/bin/picard.jar
+
+B<--java_path>
+	Optiona. Path to JAVA executable from Java 8 JDK.  If not provided, will use /usr/bin/java
 
 B<--log,-l>
     Logfile.
@@ -52,28 +91,67 @@ use strict;
 use warnings;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev pass_through);
 use Pod::Usage;
+use List::Util;
 
 ############# GLOBALS AND CONSTANTS ################
 my $debug = 1;
 my ($ERROR, $WARN, $DEBUG) = (1,2,3);
 my $logfh;
+
+my $JAVA_PATH="/usr/bin/java";
+my $PICARD_JAR="/usr/local/packages/picard/bin/picard.jar";
+
+my @stringency = qw(STRICT LENIENT SILENT);
 ####################################################
 
 my %options;
 
 # Allow program to run as module for unit testing if necessary
 main() unless caller();
+exit(0);
 
 sub main {
-	my $results = GetOptions (\%options,
-                         "input_file|i=s",
-                         "output_file|o=s",
+    my $results = GetOptions (\%options,
+						 "input_file|i=s",
+						 "output_file|o=s",
+						 "id=i",
+						 "sample_name=s",
+						 "library=s",
+						 "platform=s",
+						 "platform_unit=s",
+						 "validation_stringency=s",
+						 "max_records_stored=s",
+						 "picard_jar=s",
+						 "java_path=s",
                          "log|l=s",
                          "debug|d=s",
                          "help|h"
                           );
 
     &check_options(\%options);
+
+    my %picard_args = ( 
+			'INPUT' => $options{'input_file'},
+			'OUTPUT' => $options{'output_file'},
+			'RGID' => $options{'id'},
+			'RGLB' => $options{'library'},
+			'RGPL' => $options{'platform'},
+			'RGPU' => $options{'platform_unit'},
+			'RGSM' => $options{'sample_name'},
+			'VALIDATION_STRINGENCY' => $options{'validation_stringency'},
+			'MAX_RECORDS_IN_RAM' => $options{'max_records_stored'}
+    );
+
+    # Start building the Picard tools command
+    my $cmd = $options{'java_path'}." ".$options{'picard_jar'}." AddOrReplaceReadGroups ";
+
+    # Add only passed in options to command
+    foreach my $arg (keys %options) {
+        $cmd .= "${arg}=".$options{$arg}." " if defined $options{$arg};
+    }
+
+    exec_command($cmd);
+
 }
 
 sub check_options {
@@ -88,15 +166,39 @@ sub check_options {
 
    $debug = $opts->{'debug'} if( $opts->{'debug'} );
 
-   foreach my $req ( qw(input_file output_file) ) {
+   foreach my $req ( qw(input_file output_file library sample_name platform platform_unit) ) {
        &_log($ERROR, "Option $req is required") unless( $opts->{$req} );
    }
+
+   $opts->{'java_path'} = $JAVA_PATH if !$opts->{'java_path'};
+   $opts->{'picard_jar'} = $PICARD_JAR if !$opts->{'picard_jar'};
+
+   if ( defined $opts->{'validation_stringency'} && none{$_ eq uc($opts->{'validation_stringency'})} @stringency ) {
+       &_log($ERROR, $opts->{'validation_stringency'}." is not a valid option.  Choose from 'SILENT', 'LENIENT', or 'STRICT'");
+   }
+}
+
+sub exec_command {
+	my $sCmd = shift;
+	
+	if ((!(defined $sCmd)) || ($sCmd eq "")) {
+		&_log($ERROR,"\nSubroutine::exec_command : ERROR! Incorrect command!\n");
+	}
+	
+	my $nExitCode;
+	
+	print STDERR "$sCmd\n";
+	$nExitCode = system("$sCmd");
+	if ($nExitCode != 0) {
+		&_log($ERROR, "\tERROR! Command Failed!\n\t$!\n");
+	}
+	print STDERR "\n";
 }
 
 sub _log {
    my ($level, $msg) = @_;
    if( $level <= $debug ) {
-      print STDOUT "$msg\n";
+      print STDERR "$msg\n";
    }
    print $logfh "$msg\n" if( defined( $logfh ) );
    exit(1) if( $level == $ERROR );
