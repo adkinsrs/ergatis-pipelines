fb28ed1148a654cf80571922f7275a31e47a63fa
Shaun Adkins <sadkins@som.umaryland.edu>
2017-03-23 11:08:03 -0400

    initial write-up of split_spliced_reads.pl

--- package-nicu/bin/split_spliced_reads.pl
+++ package-nicu/bin/split_spliced_reads.pl
@@ -2,14 +2,20 @@
 
 =head1 NAME
 
-perl_template.pl - Description
+split_spliced_reads.pl - Wrapper script for GATK's SplitNCigarReads utility
 
 =head1 SYNOPSIS
 
- USAGE: perl_template.pl
-       --input_file=/path/to/some/input.file
-       --output=/path/to/transterm.file
-     [ --log=/path/to/file.log
+ USAGE: add_read_group.pl
+       --input_file=/path/to/some/input.bam
+       --output_file=/path/to/output.bam
+	   --reference_file=/path/to/ref.fa
+     [ 
+	   --read_filter=ReassignOneMappingQuality
+	   --max_reads_stored=1000000
+	   --gatk_jar=/path/to/gatk.jar
+	   --java_path=/path/to/java
+	   --log=/path/to/file.log
        --debug=3
        --help
      ]
@@ -17,8 +23,25 @@ perl_template.pl - Description
 =head1 OPTIONS
 
 B<--input_file,-i>
+	Required. Path to BAM file to serve as input
 
 B<--output_file,-o>
+	Required. File name for output BAM file
+
+B<--reference_file>
+	Required.  Path to FASTA file to serve as reference
+
+B<--read_filter>
+	Optional. Reads that fail the specified filters will not be used in the analysis.  Pass in each filter as a comma-separated list.
+
+B<--max_records_stored>
+	Optional.  Number of records to store in RAM before spilling to disk.
+
+B<--gatk_jar>
+	Optional. Path to the GATK JAR file.  If not provided, will use /usr/local/packages/GATK-3.7/GenomeAnalysisTK.jar
+
+B<--java_path>
+	Optiona. Path to JAVA executable from Java 8 JDK.  If not provided, will use /usr/bin/java
 
 B<--log,-l>
     Logfile.
@@ -52,28 +75,65 @@ use strict;
 use warnings;
 use Getopt::Long qw(:config no_ignore_case no_auto_abbrev pass_through);
 use Pod::Usage;
+use List::Util;
 
 ############# GLOBALS AND CONSTANTS ################
 my $debug = 1;
 my ($ERROR, $WARN, $DEBUG) = (1,2,3);
 my $logfh;
+
+my $JAVA_PATH="/usr/bin/java";
+my $GATK_JAR="/usr/local/packages/GATK-3.7/GenomeAnalysisTK.jar";
+
+my @stringency = qw(STRICT LENIENT SILENT);
 ####################################################
 
 my %options;
 
 # Allow program to run as module for unit testing if necessary
 main() unless caller();
+exit(0);
 
 sub main {
-	my $results = GetOptions (\%options,
-                         "input_file|i=s",
-                         "output_file|o=s",
+    my $results = GetOptions (\%options,
+						 "input_file|i=s",
+						 "output_file|o=s",
+						 "reference_file|r=s",
+						 "read_filter=s",
+						 "max_reads_stored=s",
+						 "gatk_jar=s",
+						 "java_path=s",
                          "log|l=s",
                          "debug|d=s",
                          "help|h"
                           );
 
     &check_options(\%options);
+
+    my %picard_args = ( 
+			'--input_file' => $options{'input_file'},
+			'--out' => $options{'output_file'},
+			'--reference_sequence' => $options{'reference_file'},
+			'--maxReadsInMemory' => $options{'max_reads_stored'}
+    );
+
+    # Start building the Picard tools command
+    my $cmd = $options{'java_path'}." ".$options{'gatk_jar'}." --analysis_type SplitNCigarReads ";
+
+	# Split csv list of filters and add as individual options
+    if ($options{'read_filter'}) {
+		$cmd .= '--read_filter ';
+		my @filters = split(/,/, $options{'read_filter'});
+		$cmd .= join '--read_filter ', @filters;
+	}
+
+    # Add only passed in options to command
+    foreach my $arg (keys %options) {
+        $cmd .= "${arg}=".$options{$arg}." " if defined $options{$arg};
+    }
+
+    exec_command($cmd);
+
 }
 
 sub check_options {
@@ -88,15 +148,36 @@ sub check_options {
 
    $debug = $opts->{'debug'} if( $opts->{'debug'} );
 
-   foreach my $req ( qw(input_file output_file) ) {
+   foreach my $req ( qw(input_file output_file reference_file) ) {
        &_log($ERROR, "Option $req is required") unless( $opts->{$req} );
    }
+
+   $opts->{'java_path'} = $JAVA_PATH if !$opts->{'java_path'};
+   $opts->{'gatk_jar'} = $GATK_JAR if !$opts->{'gatk_jar'};
+
+}
+
+sub exec_command {
+	my $sCmd = shift;
+	
+	if ((!(defined $sCmd)) || ($sCmd eq "")) {
+		&_log($ERROR,"\nSubroutine::exec_command : ERROR! Incorrect command!\n");
+	}
+	
+	my $nExitCode;
+	
+	print STDERR "$sCmd\n";
+	$nExitCode = system("$sCmd");
+	if ($nExitCode != 0) {
+		&_log($ERROR, "\tERROR! Command Failed!\n\t$!\n");
+	}
+	print STDERR "\n";
 }
 
 sub _log {
    my ($level, $msg) = @_;
    if( $level <= $debug ) {
-      print STDOUT "$msg\n";
+      print STDERR "$msg\n";
    }
    print $logfh "$msg\n" if( defined( $logfh ) );
    exit(1) if( $level == $ERROR );
