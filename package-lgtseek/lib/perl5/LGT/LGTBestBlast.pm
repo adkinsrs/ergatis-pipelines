5f0e31f4af50b5c1faba2ad73e706b5f396aff74
Shaun Adkins <sadkins@som.umaryland.edu>
2017-12-15 10:09:12 -0500

    fixing syntax issues

--- package-lgtseek/lib/perl5/LGT/LGTBestBlast.pm
+++ package-lgtseek/lib/perl5/LGT/LGTBestBlast.pm
@@ -32,13 +32,13 @@ $Carp::MaxArgLen = 0;
 $|               = 1;
 
 # Globals
-my $filter_hits        = [];
-my $lineage            = [];
+my $filter_hits = [];
+my $lineage     = [];
 my $gi2tax;
 my $outfile;
 my $FILTER_MIN_OVERLAP = 50;
-my $BLAST_BIN = '/usr/local/bin/blastall';
-my $BLAST_CMD_ARGS = ' -p blastn -e 1 -m8';
+my $BLAST_BIN          = '/usr/local/bin/blastall';
+my $BLAST_CMD_ARGS     = ' -p blastn -e 1 -m8';
 my $filter_lineage;
 my $min_overlap;
 
@@ -65,19 +65,20 @@ my $min_overlap;
 =cut
 
 sub filterBlast {
-    my ($class, $args) = @_;
+    my ( $class, $args ) = @_;
 
 # Default filter min overlap is 50 (minimum length to filter out overlapping reads
     $min_overlap =
-      $args->{filter_min_overlap} ? $args->{filter_min_overlap} : $FILTER_MIN_OVERLAP;
+        $args->{filter_min_overlap}
+      ? $args->{filter_min_overlap}
+      : $FILTER_MIN_OVERLAP;
     $filter_lineage = $args->{filter_lineage};
 
     # Figure out what inputs we have
     my $input;
     if ( $args->{fasta} ) {
         $input = $args->{fasta};
-    }
-    elsif ( $args->{blast} ) {
+    } elsif ( $args->{blast} ) {
         $input = $args->{blast};
     }
 
@@ -94,17 +95,16 @@ sub filterBlast {
 
     &_init_lineage( $args, $name );
 
-    $args->{blast_bin} = $BLAST_BIN if !defined ($args->{blast_bin});
+    $args->{blast_bin} = $BLAST_BIN if !defined( $args->{blast_bin} );
     my $fh;
-	  # Use blast results if already provided, otherwise run blast to get results
+
+    # Use blast results if already provided, otherwise run blast to get results
     if ( $args->{blast} ) {
-		print STDERR "Opening blast input.\n";
+        print STDERR "Opening blast input.\n";
         open( $fh, "<$input" ) or confess "Unable to open $input\n";
-    }
-    elsif ( !$args->{blast} ) {
+    } elsif ( !$args->{blast} ) {
         open( $fh, "-|",
-"$args->{blast_bin} $BLAST_CMD_ARGS -d $args->{db} -i $input"
-          )
+            "$args->{blast_bin} $BLAST_CMD_ARGS -d $args->{db} -i $input" )
           or confess
           "Unable to run: $args->{blast_bin} on: $input with db: $args->{db}\n";
     }
@@ -114,8 +114,8 @@ sub filterBlast {
     close $lineage->{handle};
 
     return {
-        list_file     => $list,
-        outfile => $outfile,
+        list_file => $list,
+        outfile   => $outfile,
     };
 }
 
@@ -123,25 +123,24 @@ sub filterBlast {
 sub _init_lineage {
     my $args = shift;
     my $name = shift;
-    if ( $args->{lineage}) {
-        my $lineage_name = $args->{lineage}
+    if ( $args->{lineage} ) {
+        my $lineage_name = $args->{lineage};
         $outfile = $args->{output};
-        if ( !$args->{output1} && $args->{output_dir} ) {
+        if ( !$args->{output} && $args->{output_dir} ) {
             $outfile = $args->{output_dir} . "/$name\_$lineage_name.out";
         }
         open my $out_fh, ">$outfile"
           or confess "Unable to open lineage1 output $outfile\n";
 
-        my $lineage =
-                {
-                    'lineage'   => $args->{lineage},
-                    'best_e'    => 100,			# Dummy value so next will always be better
-					          'best_bit'	=> 0,
-                    'id'        => '',
-                    'handle'    => $out_fh,
-                    'best_rows' => [],
-                    'name'      => 'lineage'
-                };
+        my $lineage = {
+            'lineage' => $args->{lineage},
+            'best_e'    => 100,      # Dummy value so next will always be better
+            'best_bit'  => 0,
+            'id'        => '',
+            'handle'    => $out_fh,
+            'best_rows' => [],
+            'name'      => 'lineage'
+        };
     }
 }
 
@@ -149,7 +148,7 @@ sub _init_lineage {
 sub _process_file {
     my $fh = shift;
     use Data::Dumper;
-    while (my $line = <$fh>) {
+    while ( my $line = <$fh> ) {
         chomp $line;
         my @fields = split( /\t/, $line );
         my $tax;
@@ -163,19 +162,22 @@ sub _process_file {
                 'lineage'  => $fields[14],
                 'name'     => $fields[13]
             };
-        }
-        else {
+        } else {
             $tax = $gi2tax->getTaxon( $fields[1] );
         }
 
-    		# If taxon info was not already in m8 file, add it
-    		if (!$found_tax) {
-    			push (@fields, ($tax->{taxon_id}, $tax->{name}, $tax->{lineage} ) );
-    		}
-    		# Die if we are missing key info
-    		carp "Unable to find name or lineage for taxon_id $tax->{'taxon_id'} in trace $fields[1]\n"
-    			unless $tax->{name};
-        carp "Unable to find taxon info for $fields[1]\n" unless $tax->{taxon_id};
+        # If taxon info was not already in m8 file, add it
+        if ( !$found_tax ) {
+            push( @fields,
+                ( $tax->{taxon_id}, $tax->{name}, $tax->{lineage} ) );
+        }
+
+        # Die if we are missing key info
+        carp
+          "Unable to find name or lineage for taxon_id $tax->{'taxon_id'} in trace $fields[1]\n"
+          unless $tax->{name};
+        carp "Unable to find taxon info for $fields[1]\n"
+          unless $tax->{taxon_id};
 
         my $fields_w_trace = &_add_trace_info( \@fields );
 
@@ -195,38 +197,42 @@ sub _process_line {
 
     my $finished_id = 0;
 
-	# If our best lineage ID has been identified as the current query ID...
-	if ( $lineage->{id} eq $fields->[0] ) {
-		# SAdkins - 11/20/17 - Changed from using best e-val to best bit score instead
-		# Determining how to handle equal or better hits
-		if ( $fields->[11] == $lineage->{best_bit} ) {
-				# If our hit is equally as good as our best, then allow for more than one best hit
-				push( @{ $lineage->{best_rows} }, $fields );
-		} elsif ( $fields->[11] > $lineage->{best_bit} ) {
-				$lineage->{best_bit}  = $fields->[11];
-				$lineage->{best_rows} = [$fields];
-		}
-  } else {    # If we are ready to move on to the next Query ID
-		# We have finished a hit
-		&_print_hits($lineage);
-		$finished_id = 1;
+    # If our best lineage ID has been identified as the current query ID...
+    if ( $lineage->{id} eq $fields->[0] ) {
 
-		# Store new hit as the first 'best hit'.
-		$lineage->{id}        = $fields->[0];
-		$lineage->{best_e}    = $fields->[10];
-		$lineage->{best_bit}  = $fields->[11];
-		$lineage->{best_rows} = [$fields];
+  # SAdkins - 11/20/17 - Changed from using best e-val to best bit score instead
+  # Determining how to handle equal or better hits
+        if ( $fields->[11] == $lineage->{best_bit} ) {
 
-    if ( &_filter_hit( $tax->{name} ) ) {
-        push( @$filter_hits, $fields );
+# If our hit is equally as good as our best, then allow for more than one best hit
+            push( @{ $lineage->{best_rows} }, $fields );
+        } elsif ( $fields->[11] > $lineage->{best_bit} ) {
+            $lineage->{best_bit}  = $fields->[11];
+            $lineage->{best_rows} = [$fields];
+        }
+    } else {    # If we are ready to move on to the next Query ID
+                # We have finished a hit
+        &_print_hits($lineage);
+        $finished_id = 1;
+
+        # Store new hit as the first 'best hit'.
+        $lineage->{id}        = $fields->[0];
+        $lineage->{best_e}    = $fields->[10];
+        $lineage->{best_bit}  = $fields->[11];
+        $lineage->{best_rows} = [$fields];
+
+        if ( &_filter_hit( $tax->{name} ) ) {
+            push( @$filter_hits, $fields );
+        }
+        return $finished_id;
     }
-    return $finished_id;
 }
 
 # Print out the best hit for this particular query ID.
 sub _print_hits {
     my $lineage = shift;
     if ( $lineage->{id} ) {
+
         # We have finished a hit
         if ( scalar @{ $lineage->{best_rows} } ) {
             if ( !&_filter_best_hits( $lineage->{best_rows} ) ) {
@@ -252,15 +258,15 @@ sub _add_trace_info {
     # Ghetto way of checking for directionality.
     my $dir = 'F';
     if ( $id =~ /(.*)[\_\/](\d)/ ) {
-	# Determine forward and reverse by which mate pair it is
+
+        # Determine forward and reverse by which mate pair it is
         if ( $2 == 1 ) {
             push( @$list, ( $1, 'F' ) );
-        }
-        elsif ( $2 == 2 ) {
+        } elsif ( $2 == 2 ) {
             push( @$list, ( $1, 'R' ) );
-        }
-        else {
-            print STDERR "Couldn't figure out the clone name from $list->[0] assuming F\n";
+        } else {
+            print STDERR
+              "Couldn't figure out the clone name from $list->[0] assuming F\n";
             push( @$list, ( $1, 'F' ) );
         }
     }
@@ -318,10 +324,10 @@ sub _create_list_of_outputs {
     my $out_dir;
     my ( $name, $directories, $suffix );
     if ( $config->{output_dir} ) {
-		$out_dir = $config->{output_dir};
-	}
+        $out_dir = $config->{output_dir};
+    }
 
-	if ( $config->{fasta} ) {
+    if ( $config->{fasta} ) {
         ( $name, $directories, $suffix ) =
           fileparse( $config->{fasta}, qr/\.[^.]*/ );
         $out_dir = $directories if !defined $out_dir;
