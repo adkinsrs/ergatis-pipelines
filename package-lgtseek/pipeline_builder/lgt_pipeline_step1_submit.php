207518cb10d67a45851c20d1f487467b19087a9d
Shaun Adkins <sadkins@som.umaryland.edu>
2018-03-03 18:48:19 -0500

    reading accession lists from Docker from /mnt/input_data/acc_list

--- package-lgtseek/pipeline_builder/lgt_pipeline_step1_submit.php
+++ package-lgtseek/pipeline_builder/lgt_pipeline_step1_submit.php
@@ -87,12 +87,12 @@
 
 		if ( !empty($_POST['tbac_acc']) ) {
 			$bac_acc = trim($_POST['tbac_acc']);
-			$bac_acc = adjust_paths($bac_acc, $dir, "/mnt/input_data/", false);
+			$bac_acc = adjust_paths($bac_acc, $dir, "/mnt/input_data/acc_list", false);
 			$args .= "--bac_accession $bac_acc ";
 		}
 		if ( !empty($_POST['teuk_acc']) ) {
 			$euk_acc = trim($_POST['teuk_acc']);
-			$euk_acc = adjust_paths($euk_acc, $dir, "/mnt/input_data/", false);
+			$euk_acc = adjust_paths($euk_acc, $dir, "/mnt/input_data/acc_list", false);
 			$args .= "--euk_accession $euk_acc ";
 		}
 		if ( !empty($_POST['tbac_ref']) ) {
